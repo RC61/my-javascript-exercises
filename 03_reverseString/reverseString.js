3a3ce0b11320e250b89156bb5f0a945ed2b3597b
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-09 07:35:44 -0500

    solve for all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(testInput) {
+    let testOutput = "";
+    for (let i = testInput.length -1; i >= 0; i--) {
+        testOutput += testInput[i];
+    }
+    return testOutput;
 };
 
 // Do not edit below this line
