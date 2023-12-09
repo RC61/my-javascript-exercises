528620c4cb95d145a4729eff3b8fd57c0ad90a42
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-09 07:18:00 -0500

    successfully pass all tests for 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
 const repeatString = function(word, numberOfTimes) {
     let repeatWord = word;
 
+    if (numberOfTimes == 0){
+        return "";
+    } else if (numberOfTimes < 0){
+        return "ERROR";
+    }
+    
     for (let i = 1; i < numberOfTimes; i++) {
         repeatWord += word;
      }
