bf567443a10efde033b2401ff94b8b8b6468f870
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-09 07:12:01 -0500

    solve for first 3 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(word, numberOfTimes) {
+    let repeatWord = word;
 
+    for (let i = 1; i < numberOfTimes; i++) {
+        repeatWord += word;
+     }
+    return repeatWord;
 };
 
 // Do not edit below this line
