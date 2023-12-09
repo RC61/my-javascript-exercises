b9a5c52937447fb6ae6503ac55734d676c2638b0
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-09 11:01:29 -0500

    add ability to pass in test array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputValues, numToPop) {
+const functionArray = [];
 
-};
+// I have to first pass the input values into another array
+// ok, but how?
+
+let i = inputValues.length; //(for me) sets i to length of input array
+
+for (i; i > 0; i-- ){
+        let torpedoBay = inputValues.shift(); //(for me) inits torpedoBay to first value of inputVal array
+        functionArray.push(torpedoBay); // (for me) takes current torpedoBay value and pushes it to first spot on myArray
+}
+
+console.log(functionArray);
+return functionArray;
+// let i = inputValues.length;
+// for (i; i > 0; i--){
+//     functionArray.map(inputValues[i]);
+//     inputValues.shift[0];
+// }
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
