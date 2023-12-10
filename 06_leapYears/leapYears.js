9a41f9be18a0d1a14d65776596ec56527f78d67b
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-10 09:24:06 -0500

    provide if statements that solve for first 3 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   let yearSwitch;
+    if (year % 4 == 0){
+        console.log("fuck you " + year)
+        yearSwitch = true;
+        return yearSwitch;
+    } else {
+        yearSwitch = false;
+        return yearSwitch;
+    }
 };
 
 // Do not edit below this line
