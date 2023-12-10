b4bf97a4bff772ff868fb19fec8fbd198f2f9cf0
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-10 09:31:33 -0500

    add if statements that solve for exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
 const leapYears = function(year) {
    let yearSwitch;
-    if (year % 4 == 0){
+    if (year % 100 == 0){
+        if (year % 400 == 0){
+            console.log("congrats " + year)
+            yearSwitch = true;
+            return yearSwitch;
+        }
+        console.log("get fucked " + year)
+        yearSwitch = false;
+        return yearSwitch;
+    } else if (year % 4 == 0){
         console.log("fuck you " + year)
         yearSwitch = true;
         return yearSwitch;
