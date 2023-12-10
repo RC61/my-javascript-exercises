c1ae00cf103f0a24e33f3cc276fb65202e74f61e
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-10 09:04:34 -0500

    add the hardest hitting spaghetti code there ever was. solves exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,10 +3,13 @@ const sumAll = function(paramOne , paramTwo) {
     console.log(paramOne + paramTwo);
 
 
-    if (paramOne < 0 || paramTwo < 0 || paramOne == NaN || paramTwo == NaN){
+    if (paramOne < 0 || paramTwo < 0 || typeof paramOne == "string" || typeof paramTwo == "string"){
         let oopsies = "ERROR";
         return oopsies;
-    } else if(paramOne > paramTwo){
+    } else if (typeof paramOne == "object" || typeof paramTwo == "object"){
+        let oopsies = "ERROR";
+        return oopsies;
+    }else if(paramOne > paramTwo){
         console.log("Execute order 66")
         for (let i = (paramOne - paramTwo); i >= 0; i-- ){
             countTotal += paramOne;
