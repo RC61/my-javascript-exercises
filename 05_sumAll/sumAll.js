995d463bf662f0b4b820f697010544e274514a29
RC61 <51101973+RC61@users.noreply.github.com>
2023-12-10 08:47:16 -0500

    add loops that solve for first 4 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(paramOne , paramTwo) {
+    let countTotal = 0;
+    console.log(paramOne + paramTwo);
 
-};
+
+    if (paramOne < 0 || paramTwo < 0 || paramOne == NaN || paramTwo == NaN){
+        let oopsies = "ERROR";
+        return oopsies;
+    } else if(paramOne > paramTwo){
+        console.log("Execute order 66")
+        for (let i = (paramOne - paramTwo); i >= 0; i-- ){
+            countTotal += paramOne;
+            // console.log("count total is: " + countTotal);
+            --paramOne;
+        }
+        return countTotal;
+
+    } else if(paramOne < paramTwo){
+        console.log("Execute order 99")
+        for (let i = (paramTwo - paramOne); i >= 0; i-- ){
+            countTotal += paramTwo;
+            // console.log("count total is: " + countTotal);
+            --paramTwo;
+        }
+        return countTotal;
+    
+    }
+  
+}
 
 // Do not edit below this line
 module.exports = sumAll;
